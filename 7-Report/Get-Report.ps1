d5dc52c9ffde5a119ad6a7740d572da96ea5d16a
Jan Faurskov <22591930+jfaurskov@users.noreply.github.com>
2025-09-25 14:49:01 +0200

    updated version with readme

--- 7-Report/Get-Report.ps1
+++ 7-Report/Get-Report.ps1
@@ -1,42 +1,58 @@
 <#
 .SYNOPSIS
-    Exports Azure resource availability comparison between regions to Excel or CSV.
+    Exports Azure resource availability and cost comparison between regions to Excel
 
 .DESCRIPTION
-    Reads the output from Get-AvailabilityInformation.ps1, structures it, and
-    exports to an Excel or CSV file, including SKU details.
+    Reads the output from 2-AvailabilityCheck/Get-Region.ps1 and 3-CostInformation/Perform-RegionComparison.ps1, structures it, and
+    exports to an Excel file, including SKU details.
 
-.PARAMETER InputPath
-    Path to the JSON or CSV file containing availability information.
-
-.PARAMETER OutputPath
-    Path where the report should be saved (without extension).
-
-.PARAMETER ExportExcel
-    If specified, exports to .xlsx (requires ImportExcel module), otherwise .csv.
+.PARAMETER availabilityInfoPath
+    Array of paths to JSON files containing availability information.
+.PARAMETER costComparisonPath
+    Path to the JSON file containing cost comparison information.
 #>
-
-## TODO:
-# Is available in implementedSkus logic does not seem to be working as expected
-# Add logic for cost (separate code))
 
 param(
     [Parameter(Mandatory = $false)][array]$availabilityInfoPath,
-    [Parameter(Mandatory = $false)][string]$costComparisonPath = "..\3-CostInformation\region_comparison_prices.json"
+    [Parameter(Mandatory = $false)][string]$costComparisonPath
 )
+
+Function Set-ColumnColor {
+    param(
+        [Parameter(Mandatory = $true)] [object]$startColumn,
+        [Parameter(Mandatory = $true)] [string]$cellValPositive,
+        [Parameter(Mandatory = $true)] [string]$cellValNegative
+    )
+    for ($col = $startColumn; $col -le $colCount; $col++) {
+        $colLetter = [OfficeOpenXml.ExcelCellAddress]::GetColumnLetter($col)
+        $cell = $ws.Cells["$colLetter$row"]
+        if ($cell.Value -eq $cellValPositive) {
+            $cell.Style.Fill.PatternType = [OfficeOpenXml.Style.ExcelFillStyle]::Solid
+            $cell.Style.Fill.BackgroundColor.SetColor([System.Drawing.Color]::LightGreen)
+        }
+        elseif ($cell.Value -eq $cellValNegative) {
+            $cell.Style.Fill.PatternType = [OfficeOpenXml.Style.ExcelFillStyle]::Solid
+            $cell.Style.Fill.BackgroundColor.SetColor([System.Drawing.Color]::LightCoral)
+        }
+    }
+}
 
 Function New-Worksheet {
     param (
-        [string]$WorksheetName,
-        [int]$LastColumnNumber
+        [Parameter(Mandatory = $true)][string]$WorksheetName,
+        [Parameter(Mandatory = $true)][int]$LastColumnNumber,
+        [Parameter(Mandatory = $true)][array]$reportData,
+        [Parameter(Mandatory = $false)][int]$startColumnNumber,
+        [Parameter(Mandatory = $false)][string]$cellValPositive,
+        [Parameter(Mandatory = $false)][string]$cellValNegative
     )
-$excelParams = @{
-    Path          = $xlsxFileName
-    WorksheetName = $WorksheetName
-    AutoSize      = $true
-    TableStyle    = 'None'
-    PassThru      = $true
-}
+    $excelParams = @{
+        Path          = $xlsxFileName
+        WorksheetName = $WorksheetName
+        AutoSize      = $true
+        TableStyle    = 'None'
+        PassThru      = $true
+    }
     $excelPkg = $reportData | Select-Object -Property $allProps | Export-Excel @excelParams
     $ws = $excelPkg.Workbook.Worksheets[$WorksheetName]
     $lastColLetter = [OfficeOpenXml.ExcelCellAddress]::GetColumnLetter($lastColumnNumber)
@@ -47,144 +63,126 @@ $excelParams = @{
     for ($row = 2; $row -le ($reportData.Count + 1); $row++) {
         # Get the total number of columns in the worksheet
         $colCount = $ws.Dimension.Columns
-        for ($col = 5; $col -le $colCount; $col++) {
-            # Column 5 is E
-            $colLetter = [OfficeOpenXml.ExcelCellAddress]::GetColumnLetter($col)
-            $cell = $ws.Cells["$colLetter$row"]
-            if ($cell.Value -eq "Available") {
-                $cell.Style.Fill.PatternType = [OfficeOpenXml.Style.ExcelFillStyle]::Solid
-                $cell.Style.Fill.BackgroundColor.SetColor([System.Drawing.Color]::LightGreen)
-            }
-            elseif ($cell.Value -eq "Not available") {
-                $cell.Style.Fill.PatternType = [OfficeOpenXml.Style.ExcelFillStyle]::Solid
-                $cell.Style.Fill.BackgroundColor.SetColor([System.Drawing.Color]::LightCoral)
-            }
+        # Call the function to set column colors based on cell values
+        If($startColumnNumber) {
+            Set-ColumnColor -startColumn $startColumnNumber -cellValPositive $cellValPositive -cellValNegative $cellValNegative
         }
     }
     $excelPkg.Save()
+    "Sheet '$WorksheetName' with $($reportData.Count) entries added to '$xlsxFileName'."
 }
 
-# Consider splitting into functions for better readability and maintainability
-$reportData = @()
-foreach ($path in $availabilityInfoPath) {
-    $rawdata = Get-Content $path | ConvertFrom-Json -Depth 10
-    foreach ($item in $rawdata) {
-        $implementedSkus = ""
-        # if implementedSkus is exists and is not null
-        if ($item.ImplementedSkus -and $item.ImplementedSkus.Count -gt 0) {
-            $resourceType = $item.ResourceType
-            ForEach ($sku in $item.ImplementedSkus) {
-                # Customize output based on ResourceType
-                switch ($resourceType) {
-                    "microsoft.compute/virtualmachines" { $resourceType; $implementedSkus += $sku.vmSize + "," }
-                    default { 
-                        $resourceType; $implementedSkus += $sku.name + "," 
-                    } # No action for other resource types
-                }
+# Collect all property names in first-seen order
+Function Get-Props {
+    param (
+        [array]$data
+    )
+    $allProps = @()
+    foreach ($obj in $data) {
+        foreach ($p in $obj.PSObject.Properties.Name) {
+            if ($allProps -notcontains $p) {
+                $allProps += $p
             }
         }
-        else {
-            $implementedSkus += "N/A"
-        }
-        $implementedSkus = $implementedSkus.TrimEnd(",")
-        $regionAvailability = "Not available"
-        $regionHeader = $item.SelectedRegion.region
-        If ($item.SelectedRegion.available) {
-            $regionAvailability = "Available"
-        }
-        # If an object with this resource type already exists in reportData, update it
-        if ($reportData | Where-Object { $_.ResourceType -eq $item.ResourceType }) {
-            # If it exists, update the existing object with the new region availability
-            $existingItem = $reportData | Where-Object { $_.ResourceType -eq $item.ResourceType }
-            $existingItem | Add-Member -MemberType NoteProperty -Name $regionHeader -Value $regionAvailability
-        }
-        else {
-            $reportItem = [PSCustomObject]@{
-                ResourceType       = $item.ResourceType
-                ResourceCount      = $item.ResourceCount
-                ImplementedRegions = ($item.ImplementedRegions -join ", ")
-                ImplementedSkus    = $implementedSkus
-                $regionHeader      = $regionAvailability
-            }
-            $reportData += $reportItem
-        }
     }
-}
-# $costComparisonPath = "..\3-CostInformation\region_comparison_prices.json"
-$rawdata = Get-Content $costComparisonPath | ConvertFrom-Json -Depth 10
-$reportData = @()
-$uniqueMeterIds = $rawdata | Select-Object -Property OrigMeterId -Unique
-foreach ($meterId in $uniqueMeterIds) {
-    $meterId = $meterId.OrigMeterId
-    # get all occurrences of this meterId in $rawdata
-    $meterOccurrences = $rawdata | Where-Object { $_.OrigMeterId -eq $meterId }
-    $meterOccurrences
-    $basedata = $meterOccurrences | Select-Object -Property ServiceName, MeterName, ProductName, SKUName -Unique
-    $serviceName = $basedata.ServiceName
-    $meterName = $basedata.MeterName
-    $productName = $basedata.ProductName
-    $skuName = $basedata.SKUName
-    $pricingObj = [PSCustomObject]@{}
-    foreach ($occurrence in $meterOccurrences) {
-        $region = $occurrence.Region
-        if ($region -eq $null -or $region -eq "") {
-            $region = "Global"
-        }
-        "region is $region"
-
-        $retailPrice = $occurrence.RetailPrice
-        $priceDiffToOrigin = $occurrence.PriceDiffToOrigin
-        $pricingObj | Add-Member -MemberType NoteProperty -Name "$region-RetailPrice" -Value $retailPrice
-        $pricingObj | Add-Member -MemberType NoteProperty -Name "$region-PriceDiffToOrigin" -Value $priceDiffToOrigin
-    }
-    # Create a new object for each unique meter ID
-    $costReportItem = [PSCustomObject]@{
-        MeterId     = $meterId
-        ServiceName = $serviceName
-        MeterName   = $meterName
-        ProductName = $productName
-        SKUName     = $skuName
-    } 
-    Foreach ($key in $pricingObj.PSObject.Properties.Name) {
-        $costReportItem | Add-Member -MemberType NoteProperty -Name $key -Value $pricingObj.$key
-    }   
-        
-    # Add the cost report item to the report data array
-    $reportData += $costReportItem      
+    return $allProps
 }
 
 #Define output file name with current timestamp (yyyyMMdd_HHmmss)
 $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
-$csvFileName = "Availability_Report_$timestamp.csv"
 $xlsxFileName = "Availability_Report_$timestamp.xlsx"
 
-# $excelParams = @{
-#     Path          = $xlsxFileName
-#     WorksheetName = "General"
-#     AutoSize      = $true
-#     TableStyle    = 'None'
-#     PassThru      = $true
-# }
-
-#$allProps = $reportData | ForEach-Object { $_.PSObject.Properties.Name } | Sort-Object -Unique
-# Collect all property names in first-seen order
-$allProps = @()
-foreach ($obj in $reportData) {
-    foreach ($p in $obj.PSObject.Properties.Name) {
-        if ($allProps -notcontains $p) {
-            $allProps += $p
+If ($availabilityInfoPath) {
+    # Consider splitting into functions for better readability and maintainability
+    $reportData = @()
+    foreach ($path in $availabilityInfoPath) {
+        $rawdata = Get-Content $path | ConvertFrom-Json -Depth 10
+        foreach ($item in $rawdata) {
+            $implementedSkus = ""
+            # if implementedSkus is exists and is not null
+            if ($item.ImplementedSkus -and $item.ImplementedSkus.Count -gt 0) {
+                $resourceType = $item.ResourceType
+                ForEach ($sku in $item.ImplementedSkus) {
+                    # Customize output based on ResourceType
+                    switch ($resourceType) {
+                        "microsoft.compute/virtualmachines" { $implementedSkus += $sku.vmSize + "," }
+                        default { $implementedSkus += $sku.name + "," }
+                    }
+                }
+            }
+            else {
+                $implementedSkus += "N/A"
+            }
+            $implementedSkus = $implementedSkus.TrimEnd(",")
+            $regionAvailability = "Not available"
+            $regionHeader = $item.SelectedRegion.region
+            If ($item.SelectedRegion.available -eq "true") {
+                $regionAvailability = "Available"
+            }
+            # If an object with this resource type already exists in reportData, update it
+            if ($reportData | Where-Object { $_.ResourceType -eq $item.ResourceType }) {
+                # If it exists, update the existing object with the new region availability
+                $existingItem = $reportData | Where-Object { $_.ResourceType -eq $item.ResourceType }
+                $existingItem | Add-Member -MemberType NoteProperty -Name $regionHeader -Value $regionAvailability
+            }
+            else {
+                $reportItem = [PSCustomObject]@{
+                    ResourceType       = $item.ResourceType
+                    ResourceCount      = $item.ResourceCount
+                    ImplementedRegions = ($item.ImplementedRegions -join ", ")
+                    ImplementedSkus    = $implementedSkus
+                    $regionHeader      = $regionAvailability
+                }
+                $reportData += $reportItem
+            }
         }
     }
+    $WorksheetName = "ServiceAvailability"
+    $allProps = Get-Props -data $reportData
+    $lastColumnNumber = $allProps.Count
+    New-Worksheet -WorksheetName $WorksheetName -LastColumnNumber $lastColumnNumber -reportData $reportData -startColumnNumber 5 -cellValPositive "Available" -cellValNegative "Not available"
 }
 
-# Export to CSV
-$reportData | Select-Object -Property $allProps | Export-Csv -Path $csvFileName -NoTypeInformation
-
-# Make the Excel first row (header) with blue background and white text
-
-$WorksheetName = "Cost"
-$lastColumnNumber = $allProps.Count
-
-New-Worksheet -WorksheetName $WorksheetName -LastColumnNumber $lastColumnNumber
-
-
+If ($costComparisonPath) {
+    $rawdata = Get-Content $costComparisonPath | ConvertFrom-Json -Depth 10
+    $costReportData = @()
+    $uniqueMeterIds = $rawdata | Select-Object -Property OrigMeterId -Unique
+    foreach ($meterId in $uniqueMeterIds) {
+        $meterId = $meterId.OrigMeterId
+        # get all occurrences of this meterId in $rawdata
+        $meterOccurrences = $rawdata | Where-Object { $_.OrigMeterId -eq $meterId }
+        $basedata = $meterOccurrences | Select-Object -Property ServiceName, MeterName, ProductName, SKUName -Unique
+        $serviceName = $basedata.ServiceName
+        $meterName = $basedata.MeterName
+        $productName = $basedata.ProductName
+        $skuName = $basedata.SKUName
+        $pricingObj = [PSCustomObject]@{}
+        foreach ($occurrence in $meterOccurrences) {
+            $region = $occurrence.Region
+            if ($null -eq $region -or $region -eq "") {
+                $region = "Global"
+            }
+            $retailPrice = $occurrence.RetailPrice
+            $priceDiffToOrigin = $occurrence.PriceDiffToOrigin
+            $pricingObj | Add-Member -MemberType NoteProperty -Name "$region-RetailPrice" -Value $retailPrice
+            $pricingObj | Add-Member -MemberType NoteProperty -Name "$region-PriceDiffToOrigin" -Value $priceDiffToOrigin
+        }
+        # Create a new object for each unique meter ID
+        $costReportItem = [PSCustomObject]@{
+            MeterId     = $meterId
+            ServiceName = $serviceName
+            MeterName   = $meterName
+            ProductName = $productName
+            SKUName     = $skuName
+        } 
+        Foreach ($key in $pricingObj.PSObject.Properties.Name) {
+            $costReportItem | Add-Member -MemberType NoteProperty -Name $key -Value $pricingObj.$key
+        }
+        # Add the cost report item to the report data array
+        $costReportData += $costReportItem
+    }
+    $WorksheetName = "CostComparison"
+    $allProps = Get-Props -data $costReportData
+    $lastColumnNumber = $allProps.Count
+    New-Worksheet -WorksheetName $WorksheetName -LastColumnNumber $lastColumnNumber -reportData $costReportData
+}
--- 7-Report/readme.md
+++ 7-Report/readme.md
@@ -1,24 +1,40 @@
 # Export Script
 
-This script generates formatted Excel (`.xlsx`) and CSV reports based on the output from the previous check script. The reports provide detailed information for each service, including:
+This script generates formatted Excel (`.xlsx`)reports based on the output from the previous check script. The reports provide detailed information for each service, including:
+
+## Service Availability Report
 
 - **Resource type**
 - **Resource count**
 - **Implemented (origin) regions**
 - **Implemented SKUs**
-- **Selected (target) region**
-- **Availability in the selected region**
+- **Selected (target) regions**
+- **Availability in the selected regions**
 
-These reports help you analyze service compatibility across different regions.
+## Cost Comparison Report
 
-## Usage
+- **Azure Cost Meter ID**
+- **Service Name**
+- **Meter Name**
+- **Product Name**
+- **SKU Name**
+- **Retail Price per region**
+- **Price Difference to origin region per region**
+
+These reports help you analyze service compatibility and cost differences across different regions.
+
+## Dependencies
+
+- This script requires the `ImportExcel` PowerShell module. 
+- The script requires you to have run either the `2-AvailabilityCheck/Get-Region.ps1` or `3-CostInformation/Perform-RegionComparison.ps1` or both scripts to generate the necessary JSON input files for availability and cost data.
+
+## Usage Instructions
 
 1. Open a PowerShell command line.
-2. Navigate to the `7-Export` folder.
-3. Run the script:
+2. Navigate to the `7-Report` folder.
+3. If you have created one or more availability JSON files using the `2-AvailabilityCheck/Get-Region.ps1` script, run the following commands, replacing the path with your actual file path(s):
 
     ```powershell
-    .\Get-Report.ps1 -InputPath "..\2-AvailabilityCheck\Availability_Mapping_Asia_Pacific.json"
+    .\Get-Report.ps1 -availabilityInfoPath `@("..\2-AvailabilityCheck\Availability_Mapping_Asia_Pacific.json", "..\2-AvailabilityCheck\Availability_Mapping_Europe.json")` -costComparisonPath "..\3-CostInformation\region_comparison_prices.json"
     ```
-
-The script generates `.xlsx` and `.csv` files in the `7-Export` folder, named `Availability_Report_CURRENTTIMESTAMP`.
+The script generates an `.xlsx` and `.csv` files in the `7-report` folder, named `Availability_Report_CURRENTTIMESTAMP`.
